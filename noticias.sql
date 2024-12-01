abfaa16c158a0d21e67207cf023eb977b87ea9b7
Segundo Galdames <segundogaldames@gmail.com>
2024-12-01 20:24:43 -0300

    Respaldo base de datos

--- noticias.sql
+++ noticias.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.39, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.40, for Linux (x86_64)
 --
 -- Host: localhost    Database: noticias
 -- ------------------------------------------------------
--- Server version	8.0.39-0ubuntu0.24.04.2
+-- Server version	8.0.40-0ubuntu0.24.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,6 +14,36 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `noticias`
+--
+
+DROP TABLE IF EXISTS `noticias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `noticias` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) NOT NULL,
+  `descripcion` varchar(255) NOT NULL,
+  `vigente` tinyint NOT NULL,
+  `ruta` varchar(255) NOT NULL,
+  `usuario_id` int NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `noticias`
+--
+
+LOCK TABLES `noticias` WRITE;
+/*!40000 ALTER TABLE `noticias` DISABLE KEYS */;
+INSERT INTO `noticias` VALUES (1,'Noticia Uno','Noticia Uno',1,'noticia-uno',3,'2024-11-26 10:10:04','2024-11-26 12:40:44'),(2,'Por qué lo usamos','Por qué lo usamos',1,'por-que-lo-usamos',3,'2024-11-26 12:31:28','2024-11-26 12:45:41'),(3,'Fiscal asegura que Boric tiene calidad de imputado en investigación','Fiscal asegura que Boric tiene calidad de imputado en investigación',2,'fiscal-asegura-que-boric-tiene-calidad-de-imputado-en-investigacion',2,'2024-11-26 13:07:10','2024-11-26 13:08:08');
+/*!40000 ALTER TABLE `noticias` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `roles`
@@ -28,7 +58,7 @@ CREATE TABLE `roles` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,8 +67,39 @@ CREATE TABLE `roles` (
 
 LOCK TABLES `roles` WRITE;
 /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES (1,'Administrador','2024-11-12 23:29:23','2024-11-12 23:29:23'),(2,'Supervisor','2024-11-12 23:31:07','2024-11-12 23:31:07'),(3,'Periodistas','2024-11-12 23:31:58','2024-11-13 00:19:01');
+INSERT INTO `roles` VALUES (1,'Administrador','2024-11-12 23:29:23','2024-11-12 23:29:23'),(2,'Lector','2024-11-12 23:31:07','2024-11-22 20:01:06'),(3,'Periodista','2024-11-12 23:31:58','2024-11-17 15:51:09'),(4,'Editor','2024-11-17 15:51:26','2024-11-22 20:00:42');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `run` varchar(255) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `activo` tinyint NOT NULL,
+  `role_id` int NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'10625357-9','Juan Perez','jperezm@hotmail.com','081fc56519489f782f00a3976c862e083a62d1b7b52afe968b72fa75c9600f4a4fa7f1352990454b8cab4425229b3c0e3b587430c9543f3625bb5dde41dac481',1,4,'2024-11-17 21:55:41','2024-11-22 20:16:26'),(2,'9534876-9','Laura Miranda Torres','lmirandat@gmail.com','8b516dd044cf92092b71b1b40b0360b5f9c603bada5499f97c8d2ed0946ff00c0a5d26d593b1ca96a8685d3e7d2d52f3142d2821c29e0a4dc9e8b959cf6f29f5',1,3,'2024-11-17 22:23:12','2024-11-26 13:04:47'),(3,'13568793-6','Segundo Galdames','segundogaldames@hotmail.com','3a437e24eff73ac0cd39c72fb37680194c5faf2e33bbbffa626eea40131f195ba067edf71a6fcd26e6095017bbe10ed119034fafe087a9f8d2dfa1918eeb18cb',1,1,'2024-11-22 18:32:05','2024-11-26 12:27:36');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -50,4 +111,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-13  0:23:00
+-- Dump completed on 2024-12-01 20:23:52
